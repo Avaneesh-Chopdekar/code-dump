c677e51e77671952d062de3acc9675787e299ef3
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2023-07-15 00:12:14 +0530

    mentioned views dir route

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
+app.set('views', './views');
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
